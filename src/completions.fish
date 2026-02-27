bb5eb81a27031d59202da8f2534cf4f943754c16
Tyler Kindy <tyler@ambrook.com>
2026-02-27 10:39:10 -0500

    Move completions script to separate file

---
+++ src/completions.fish
@@ -0,0 +1,17 @@
+complete --command wt --no-files
+
+complete --command wt --condition "__fish_use_subcommand" --arguments "close" --description "Close the current iTerm window"
+complete --command wt --condition "__fish_use_subcommand" --arguments "start" --description "Start work on a new task, reusing a worktree if available"
+complete --command wt --condition "__fish_use_subcommand" --arguments "finish" --description "Finish work and release the worktree for reuse"
+complete --command wt --condition "__fish_use_subcommand" --arguments "merge-config" --description "Interactively merge worktree config changes back to the main repo"
+complete --command wt --condition "__fish_use_subcommand" --arguments "open" --description "Open an existing worktree in iTerm"
+complete --command wt --condition "__fish_use_subcommand" --arguments "list" --description "List worktrees for the current repo"
+complete --command wt --condition "__fish_use_subcommand" --arguments "completions" --description "Generate Fish shell completions"
+
+complete --command wt --condition "__fish_seen_subcommand_from open" --arguments "(wt list 2>/dev/null | string replace --regex --filter '(\\S+)\\s+.*' '\$1')" --description "Directory"
+complete --command wt --condition "__fish_seen_subcommand_from open" --arguments "(wt list 2>/dev/null | string replace --regex --filter '\\S+\\s+(\\S+)' '\$1' | string match --invert '(available)')" --description "Branch"
+
+complete --command wt --condition "__fish_seen_subcommand_from start" --arguments "(git branch --format='%(refname:short)')" --description "Branch"
+complete --command wt --condition "__fish_seen_subcommand_from start" --short-option e --long-option existing --description "Checkout an existing branch instead of creating a new one"
+complete --command wt --condition "__fish_seen_subcommand_from start" --short-option b --long-option branch-name --exclusive --description "Use a custom branch name instead of a random one"
+complete --command wt --condition "__fish_seen_subcommand_from completions" --short-option s --long-option shell --exclusive --arguments "fish" --description "Shell type"
